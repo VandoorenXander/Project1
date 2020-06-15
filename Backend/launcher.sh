e6e25fc34d40d6864b3274ada8f9db114f1be7ad
VandoorenXander <55786846+VandoorenXander@users.noreply.github.com>
2020-06-15 19:44:50 +0200

    sh file

---
+++ Backend/launcher.sh
@@ -0,0 +1,5 @@
+    #!/bin/sh
+
+    cd /home/pi/project1/Backend
+    sudo python3 api.py
+    cd /
--- Backend/project1/config.py
+++
@@ -1,9 +0,0 @@
-[connector_python]
-user = mysql
-host = 127.0.0.1
-port = 3306
-password = MCampharos1
-database = mydb
-
-[application_config]
-driver = 'SQL Server'
